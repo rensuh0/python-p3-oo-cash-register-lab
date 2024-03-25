8ca085a09ae49cb6dab35a5ccae747ef43efc752
Najeeb <najhassanali@gmail.com>
2024-03-25 09:16:15 +0300

    Assignment done

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self):
+        self.items = []
+        self.total = 0
+        self.discount = 0
+
+    def add_item(self, item_name, price, quantity):
+        self.items.append({"item_name": item_name, "price": price, "quantity": quantity})
+        self.total += price * quantity
+
+    def calculate_total(self):
+        return self.total
+
+    def apply_discount(self, discount_percentage):
+        if discount_percentage < 0 or discount_percentage > 100:
+            return "Invalid discount percentage"
+        self.total -= (self.total * discount_percentage) / 100
+
+    def void_last_transaction(self):
+        if not self.items:
+            return "No items to void"
+        last_item = self.items.pop()
+        self.total -= last_item["price"] * last_item["quantity"]
